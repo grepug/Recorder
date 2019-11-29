05e4d9496d43049fb0f6c3228a434584591b1384
Kai Shao <grepug@163.com>
2019-11-29 12:00:29 +0800

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -5,9 +5,15 @@ declare module "@grepug/recorder" {
     bitRate: number;
     onProcess: any;
   }) => {
-    open: (success: () => void, fail: () => void) => void;
+    open: (
+      success: () => void,
+      fail: (msg: string, isUserNotAllow: boolean) => void
+    ) => void;
     start: () => void;
-    stop: (cb: (blob: Blob, duration: number) => void) => void;
+    stop: (
+      cb: (blob: Blob, duration: number) => void,
+      fail?: (msg: any) => void
+    ) => void;
     close: () => void;
   };
 
